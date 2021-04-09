86b2f7cfa52653709e8c8c65bd1c55278b0dc61b
andreybutenko <ghostmancer@gmail.com>
2021-04-08 22:39:53 -0700

    Put storytellerData in its own file

--- marinerescue-frontend/src/pages/EditorPage.js
+++ marinerescue-frontend/src/pages/EditorPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import minigames from '../components/minigames';
+import storytellerData from '../storytellerData';
 import Dropdown from '../components/Dropdown';
 import ComicView from '../components/ComicView';
 import './EditorPage.scss';
@@ -43,114 +44,11 @@ const IMAGES = {
     ],
 };
 
-const TEST2 = {
-    testScene: {
-        type: 'comic',
-        background: 'test-1',
-        nextScene: 'testScene2',
-        baseFrame: [
-            {
-                type: 'sprite',
-                image: 'strawberry',
-                x: 5,
-                y: 5,
-                size: 8,
-            },
-        ],
-        frames: [
-            [
-                {
-                    id: 1,
-                    type: 'sprite',
-                    image: 'strawberry',
-                    x: 40,
-                    y: 40,
-                    size: 20,
-                },
-                {
-                    type: 'sprite',
-                    image: 'strawberry',
-                    x: 80,
-                    y: 80,
-                    size: 8.3,
-                    flipX: true,
-                },
-            ],
-            [
-                {
-                    id: 1,
-                    type: 'sprite',
-                    image: 'strawberry',
-                    x: 20,
-                    y: 20,
-                    size: 40,
-                },
-                {
-                    type: 'sprite',
-                    image: 'strawberry',
-                    x: 80,
-                    y: 80,
-                    size: 8.3,
-                    flipX: true,
-                },
-            ],
-        ]
-    },
-    testScene2: {
-        type: 'comic',
-        background: 'test-2',
-        nextScene: 'testMinigame',
-        frames: [
-            [
-                {
-                    id: 1,
-                    type: 'sprite',
-                    image: 'strawberry',
-                    x: 40,
-                    y: 40,
-                    size: 20,
-                },
-                {
-                    type: 'sprite',
-                    image: 'strawberry',
-                    x: 80,
-                    y: 80,
-                    size: 8.3,
-                    flipX: true,
-                },
-            ],
-            [
-                {
-                    id: 1,
-                    type: 'sprite',
-                    image: 'strawberry',
-                    x: 20,
-                    y: 20,
-                    size: 20,
-                },
-                {
-                    type: 'sprite',
-                    image: 'strawberry',
-                    x: 30,
-                    y: 80,
-                    size: 8.3,
-                    flipX: true,
-                },
-            ],
-        ]
-    },
-    testMinigame: {
-        type: 'minigame',
-        nextScene: 'testScene',
-        minigame: 'DemoMinigame',
-    },
-};
-
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            storytellerData: TEST2,
+            storytellerData: storytellerData,
             currentSceneName: 'testScene',
             currentFrame: 0,
             targetSpriteIdx: -1,
