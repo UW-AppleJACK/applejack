5fc723be9c3f3ecd17c71b68c09016602d11f965
jacindaeng <jceng@uw.edu>
2021-05-05 16:57:53 -0700

    Added Iteration, Storytelling, Impact/Benefit

--- marinerescue-frontend/src/pages/CapstonePage.js
+++ marinerescue-frontend/src/pages/CapstonePage.js
@@ -76,6 +76,46 @@ class CapstonePage extends React.Component {
                         </div>
                 </div>
 
+                <div class="section background-color-grape text-left">
+                    <div class="row">
+                        <div class="column">
+                            <h1 class="color-sunshine">- Iteration - </h1>
+                        </div>
+                    </div>
+                    <p class="color-sand">Our first prototype was a comic of an animal encountering harmful debris with an interactive activity. We validated this concept with 7 kids from ages 7-12.  </p>
+                    <p class="color-sand">Through the low-fidelity prototype, we wanted to validate several concepts: kids are able to understand the story, we can teach kids to characterize debris and care about debris on the beach, kids can apply their knowledge by finding items with certain characteristics in their environment, and kids find the activity fun. To evaluate our success, we created an interview <a class="color-sand" href="https://docs.google.com/document/d/1qk3IyU7KotMuuKS6YCAqRylCI-_KK6hqUrhrkWnxnuk/edit">script</a> complete with pre-questions to understand the interviewees prior knowledge about marine animals and science research, along with post-questions and an activity to assess what they learned from the story.</p>
+                    <p class="color-sand">Interviewing the kids gave us great feedback that we took into consideration when revising our design. Many said the activity was too easy and told us they already knew this content- to this we revised our storyline to include more complex words and topics. They also said they had lots of fun with the activity so we continued to add active engagement in the stories!</p>
+                    <p class="color-sand">After several iterations incorporating the feedback, we created a high fidelity prototype with an updated story, a minigame, and a journal activity. If you’re curious, you can view our Figma prototype here:  <a class="color-sand" href="http://to.marinerescue.app/story">http://to.marinerescue.app/story</a> . We then returned to user testing to evaluate some similar concepts and a few new ones: kids are able to understand the story, we can teach kids to characterize debris and care about debris on the beach, kids have an understanding of their role and responsibility to support the marine ecosystem, and kids are able to navigate the UI of the stories. </p>
+                    <p class="color-sand">We revised our interview script to help us evaluate these concepts. In particular, we asked the kids directly what they learned from the story and from the minigame. We also revised the post-question activity to classify real marine debris with the vocabulary they learned from the story. Lastly, we asked them if they would be interested in learning about more animals. </p>
+                    <p class="color-sand">We received great feedback on our concepts to test, and also noted the kids' overall enjoyment of our animations and designs.</p>
+                </div>
+
+                <div class="section background-color-sand text-left">
+                    <div class="row">
+                        <div class="column">
+                            <h1 class="color-blue">- Storytelling - </h1>
+                        </div>
+                    </div>
+                    <p class="color-grape">For the stories, we decided to make Strawberry the Harbor Seal be a navigator for the user and guide them through a journey where they meet several of her friends and solve problems that mimic the real world.</p>
+                    <p class="color-grape">Our original storyline involved Strawberry finding her friends all sick. Strawberry and the user would then go on a detective hunt to discover that her animal friends all were harmed by marine debris. After sharing our storylines with the researchers at COASST to confirm everything was factually-correct, part of their feedback was for us to consider whether the storyline was too somber for our target user.</p>
+                    <p class="color-grape">We took this feedback into consideration and decided to revise the stories to be more uplifting by giving the user the ability to proactively help Strawberry’s friends. This would also empower users through the game to use their knowledge and action to make a difference in the real world.</p>
+                    <p class="color-grape">Through this Capstone project, our team has collaborated to create 4 unique storylines- 3 on marine debris, 1 on beach safety. We meticulously make sure that our ideas are feasible to create and implement, as well as are exciting for kids to learn. Writing is a collaborative process by all of our members and we make sure that each member and COASST researchers review the storyline before it is finalized.</p>
+                </div>
+
+                <div class="section background-color-grape text-left">
+                    <div class="row">
+                        <div class="column">
+                            <h1 class="color-sunshine">- Impact/Benefit - </h1>
+                        </div>
+                    </div>
+                    <p class="color-sand">Marine Rescue is a great resource for students to learn about the marine animals and understand the impact humans can have on these creatures. The marine debris stories take an active-learning approach to online games in order to effectively teach kids about the harmful effects of marine debris on animals. They learn research vocabulary along the way, which allows them to start making a difference from their computer by classifying real debris from Washington beaches in the debris classification game. Kids can round out their Marine Rescue adventures by learning about beach safety so they can be safe while taking action by cleaning up beaches nearby them. </p>
+                    <p class="color-sand">Overall, Marine Rescue provides a holistic solution for the three main gaps we identified in early science learning:</p>
+                    <ol />
+                        <li class="color-sand">It incorporates active learning through minigames to help kids stay engaged, while also reinforcing their new knowledge</li>
+                        <li class="color-sand">It allows kids to learn about a new science topic at home, and also gives parents an activity to engage with their kids- beach cleanups</li>
+                        <li class="color-sand">It incorporates real-world pictures of debris from COASST’s Marine Debris program and gives kids an opportunity to practice contributing to citizen science</li>
+                    <p class="color-sand">Based on the user testing we have conducted, Marine Rescue activities are well-received and is an effective early science education platform for kids and parents alike. </p>
+                </div>
 
                 <div class="section background-color-sand text-left">
                     <div class="row">
@@ -102,6 +142,8 @@ class CapstonePage extends React.Component {
                         </div>
                     </div>
                 </div>
+
+                
 
                 <div class="section section-footer">
                     <Footer></Footer>
