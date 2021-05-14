e3e450ca6d4e25ba27a6e7e2d1f39e2eaefaccaa
andreybutenko <ghostmancer@gmail.com>
2021-05-14 14:31:00 -0700

    Add code to make obstacles appear in wave racer

--- marinerescue-frontend/src/components/minigames/WaveRacer.js
+++ marinerescue-frontend/src/components/minigames/WaveRacer.js
@@ -7,7 +7,7 @@ import lighter from './sprites/sprite-lighter.png';
 import strawberry from './sprites/sprite-strawberry.png';
 
 const SPEED = 10;
-const { height, width } = useWindowDimensions();
+const OBSTACLES = ["boat", "log", "wave"]
 
 class WaveRacer extends React.Component {
     constructor(props) {
@@ -15,11 +15,9 @@ class WaveRacer extends React.Component {
       this.state = {
         isGameStarted: true,
         isGameOver: false,
-        time_start: "",
-        time_since_last_obstacle: "",
-        obstacle_type: "boat",
-        obstacleX: "200px",
-        obstacleY: "0px",
+        time_start: 0,
+        time_since_last_obstacle: 0,
+        obstacles: [],
         strawberryX: -1,
       }
       this.spawnObstacle = this.spawnObstacle.bind(this);
@@ -27,20 +25,43 @@ class WaveRacer extends React.Component {
     }
 
     spawnObstacle() {
-        let OBSTACLES = ["boat", "log", "wave"]
         let obstacle = OBSTACLES[Math.floor(Math.random() * OBSTACLES.length)]
         console.log(obstacle);
+        const xPos = Math.floor(Math.random() * 50) + 25;
+        const newObstacle = {
+            'type': obstacle,
+            'x': xPos,
+            'y': 0,
+        };
+
+        const newObstacles = JSON.parse(JSON.stringify(this.state.obstacles));
+        newObstacles.push(newObstacle);
+
         this.setState({
-                "obstacle_type": obstacle,
-                "obstacleX": "100px",
-                "obstacleY": "0px"
-            });
+            obstacles: newObstacles
+        });
     }
 
-    renderSprite() {
-        return(
-            <img class="obstacle" src={{obstacle}}/>
-        )
+    renderSprites() {
+        return (
+            <>
+                {
+                    this.state.obstacles.map((obstacle, idx) => {
+                        return (
+                            <img
+                                key={idx}
+                                class="obstacle"
+                                style={{
+                                    left: `${obstacle.x}%`,
+                                    bottom: `${obstacle.y}%`,
+                                }}
+                                src={`/sprites/sprite-${obstacle.type}.png`} 
+                                alt={`Obstacle ${obstacle.type}`} />
+                        );
+                    })
+                }
+            </>
+        );
     }
 
     rndX(offset) {
@@ -65,23 +86,28 @@ class WaveRacer extends React.Component {
 
     render() {
         return (
-            <div className="text-center">
+            <div className="text-center" id="wave-racer">
                 <button onClick={this.spawnObstacle}>Spawn Obstacle</button>
+                <div className="wave-racer-container">
+                    <div className="wave-racer-view">
+                        {this.renderSprites()}
+                    </div>
 
-                {/* <button onClick={this.props.toNext}>Next</button> */}
-                <div id="minimap">
-                    <p>Reach the <span id="emphasis">pink lighter</span> before Squawky!</p>
-                    <div id="obstacle">
-                        <img src={boat} alt="A boat"/>
-                    </div>
-                    <div id="obstacle">
-                        <img src={wave} alt="A wave"/>
-                    </div>
-                    <div id="obstacle-log">
-                        <img src={log} alt="A wooden log"/>
-                    </div>
-                    <div id="obstacle">
-                        <img src={strawberry} alt="Strawberry the Seal"/>
+                    {/* <button onClick={this.props.toNext}>Next</button> */}
+                    <div id="minimap">
+                        <p>Reach the <span id="emphasis">pink lighter</span> before Squawky!</p>
+                        <div id="obstacle">
+                            <img src={boat} alt="A boat"/>
+                        </div>
+                        <div id="obstacle">
+                            <img src={wave} alt="A wave"/>
+                        </div>
+                        <div id="obstacle-log">
+                            <img src={log} alt="A wooden log"/>
+                        </div>
+                        <div id="obstacle">
+                            <img src={strawberry} alt="Strawberry the Seal"/>
+                        </div>
                     </div>
                 </div>
             </div>
