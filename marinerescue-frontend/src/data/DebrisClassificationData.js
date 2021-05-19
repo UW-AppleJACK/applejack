f9b30419d8671a7db19761666c7003e85c86b37f
andreybutenko <ghostmancer@gmail.com>
2021-05-19 11:22:10 -0700

    Add debris classification data

---
+++ marinerescue-frontend/src/data/DebrisClassificationData.js
@@ -0,0 +1,749 @@
+const DEBRIS_CLASSIFICATION_DATA = [
+    {
+        "id": 0,
+        "url": "clear - bag 2 .png",
+        "classification": "clear"
+    },
+    {
+        "id": 1,
+        "url": "clear - bag 5.png",
+        "classification": "clear"
+    },
+    {
+        "id": 2,
+        "url": "clear - bag floppy loopy.png",
+        "classification": "clear"
+    },
+    {
+        "id": 3,
+        "url": "clear - clear bottle.png",
+        "classification": "clear"
+    },
+    {
+        "id": 4,
+        "url": "clear - pntwlsn20190508drinkcup2.png",
+        "classification": "clear"
+    },
+    {
+        "id": 5,
+        "url": "crumbly - Crumbly Float 2.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 6,
+        "url": "crumbly - Crumbly Float.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 7,
+        "url": "crumbly - Crumbly Foam Pieces.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 8,
+        "url": "crumbly - Crumbly Foam.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 9,
+        "url": "crumbly - Crumbly Insulation.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 10,
+        "url": "crumbly - Crumbly Packing Peanut.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 11,
+        "url": "crumbly - Crumbly Styrofoam.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 12,
+        "url": "crumbly - Crumbly buoy.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 13,
+        "url": "crumbly - crumbly 12.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 14,
+        "url": "crumbly - crumbly 13.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 15,
+        "url": "crumbly - crumbly 14.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 16,
+        "url": "crumbly - crumbly 15.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 17,
+        "url": "crumbly - crumbly 18.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 18,
+        "url": "crumbly - crumbly 19.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 19,
+        "url": "crumbly - crumbly 2.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 20,
+        "url": "crumbly - crumbly 20.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 21,
+        "url": "crumbly - crumbly 22.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 22,
+        "url": "crumbly - crumbly 23.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 23,
+        "url": "crumbly - crumbly 26.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 24,
+        "url": "crumbly - crumbly 29.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 25,
+        "url": "crumbly - crumbly 35.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 26,
+        "url": "crumbly - crumbly 36.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 27,
+        "url": "crumbly - crumbly 37.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 28,
+        "url": "crumbly - crumbly 38.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 29,
+        "url": "crumbly - crumbly 39.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 30,
+        "url": "crumbly - crumbly 4.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 31,
+        "url": "crumbly - crumbly 40.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 32,
+        "url": "crumbly - crumbly 41.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 33,
+        "url": "crumbly - crumbly 44.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 34,
+        "url": "crumbly - crumbly 46.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 35,
+        "url": "crumbly - crumbly 47.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 36,
+        "url": "crumbly - crumbly 48 .png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 37,
+        "url": "crumbly - crumbly 49.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 38,
+        "url": "crumbly - crumbly 7.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 39,
+        "url": "crumbly - crumbly 8.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 40,
+        "url": "crumbly - crumbly 9.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 41,
+        "url": "crumbly - floppy 49.png",
+        "classification": "crumbly"
+    },
+    {
+        "id": 42,
+        "url": "floppy - Floppy Balloon.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 43,
+        "url": "floppy - Floppy Bandange.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 44,
+        "url": "floppy - Floppy Blanket.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 45,
+        "url": "floppy - Floppy Bubble Wrap.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 46,
+        "url": "floppy - Floppy Garbage Bag.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 47,
+        "url": "floppy - Floppy Leather Strap.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 48,
+        "url": "floppy - Floppy Napkin.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 49,
+        "url": "floppy - Floppy Ribbon(1).png",
+        "classification": "floppy"
+    },
+    {
+        "id": 50,
+        "url": "floppy - Floppy Ribbon.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 51,
+        "url": "floppy - Floppy Shirt.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 52,
+        "url": "floppy - Floppy Sock.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 53,
+        "url": "floppy - Floppy Strap.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 54,
+        "url": "floppy - Floppy Straw.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 55,
+        "url": "floppy - Floppy Yellow Rope.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 56,
+        "url": "floppy - Floppy tape.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 57,
+        "url": "floppy - floppy 1.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 58,
+        "url": "floppy - floppy 10.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 59,
+        "url": "floppy - floppy 11.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 60,
+        "url": "floppy - floppy 12.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 61,
+        "url": "floppy - floppy 13.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 62,
+        "url": "floppy - floppy 14.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 63,
+        "url": "floppy - floppy 15.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 64,
+        "url": "floppy - floppy 16.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 65,
+        "url": "floppy - floppy 18.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 66,
+        "url": "floppy - floppy 19.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 67,
+        "url": "floppy - floppy 2.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 68,
+        "url": "floppy - floppy 20.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 69,
+        "url": "floppy - floppy 21.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 70,
+        "url": "floppy - floppy 22.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 71,
+        "url": "floppy - floppy 23.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 72,
+        "url": "floppy - floppy 25.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 73,
+        "url": "floppy - floppy 26.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 74,
+        "url": "floppy - floppy 27.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 75,
+        "url": "floppy - floppy 28.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 76,
+        "url": "floppy - floppy 29.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 77,
+        "url": "floppy - floppy 3.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 78,
+        "url": "floppy - floppy 30.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 79,
+        "url": "floppy - floppy 31.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 80,
+        "url": "floppy - floppy 32.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 81,
+        "url": "floppy - floppy 33.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 82,
+        "url": "floppy - floppy 35.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 83,
+        "url": "floppy - floppy 36.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 84,
+        "url": "floppy - floppy 37.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 85,
+        "url": "floppy - floppy 38.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 86,
+        "url": "floppy - floppy 4.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 87,
+        "url": "floppy - floppy 40.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 88,
+        "url": "floppy - floppy 46.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 89,
+        "url": "floppy - floppy 5.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 90,
+        "url": "floppy - floppy 50.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 91,
+        "url": "floppy - floppy 6.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 92,
+        "url": "floppy - loopy 1.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 93,
+        "url": "floppy - loopy 48.png",
+        "classification": "floppy"
+    },
+    {
+        "id": 94,
+        "url": "loopyfloppy - 6pack floppy loopy.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 95,
+        "url": "loopyfloppy - Floppy_Loopy Flip Flop.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 96,
+        "url": "loopyfloppy - Floppy_Loopy Mask.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 97,
+        "url": "loopyfloppy - Floppy_Loopy Mask2.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 98,
+        "url": "loopyfloppy - IMG_20190426_140516465_HDR.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 99,
+        "url": "loopyfloppy - Loopy 6-pack rings.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 100,
+        "url": "loopyfloppy - Loopy Caution Tape.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 101,
+        "url": "loopyfloppy - Loopy Crab Pot.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 102,
+        "url": "loopyfloppy - Loopy Fishing line.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 103,
+        "url": "loopyfloppy - Loopy Green Rope.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 104,
+        "url": "loopyfloppy - Loopy Rope.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 105,
+        "url": "loopyfloppy - Loopy bait bag.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 106,
+        "url": "loopyfloppy - Nov21_20 2_1.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 107,
+        "url": "loopyfloppy - floppy 41.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 108,
+        "url": "loopyfloppy - loopy 14.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 109,
+        "url": "loopyfloppy - loopy 23.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 110,
+        "url": "loopyfloppy - loopy 29.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 111,
+        "url": "loopyfloppy - loopy 32.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 112,
+        "url": "loopyfloppy - loopy 35.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 113,
+        "url": "loopyfloppy - loopy 40.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 114,
+        "url": "loopyfloppy - loopy 41.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 115,
+        "url": "loopyfloppy - loopy 42.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 116,
+        "url": "loopyfloppy - loopy 46.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 117,
+        "url": "loopyfloppy - loopy 47.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 118,
+        "url": "loopyfloppy - loopy 49.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 119,
+        "url": "loopyfloppy - loopy 5.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 120,
+        "url": "loopyfloppy - loopy 7.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 121,
+        "url": "loopyfloppy - loopy rope(1).png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 122,
+        "url": "loopyfloppy - rope 3.png",
+        "classification": "loopyfloppy"
+    },
+    {
+        "id": 123,
+        "url": "sharp - 20170319_162310.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 124,
+        "url": "sharp - 20191012_113158.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 125,
+        "url": "sharp - DSCF0273.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 126,
+        "url": "sharp - IMG_2113.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 127,
+        "url": "sharp - IMG_2114.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 128,
+        "url": "sharp - Sharp Boat.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 129,
+        "url": "sharp - Sharp Glass 2.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 130,
+        "url": "sharp - Sharp Glass.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 131,
+        "url": "sharp - Sharp Hand Rake.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 132,
+        "url": "sharp - Sharp Nail.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 133,
+        "url": "sharp - Sharp Pliers.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 134,
+        "url": "sharp - Sharp Skewer.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 135,
+        "url": "sharp - image002.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 136,
+        "url": "sharp - sharp 10.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 137,
+        "url": "sharp - sharp 14.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 138,
+        "url": "sharp - sharp 19.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 139,
+        "url": "sharp - sharp 27.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 140,
+        "url": "sharp - sharp 31.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 141,
+        "url": "sharp - sharp 36.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 142,
+        "url": "sharp - sharp 47.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 143,
+        "url": "sharp - sharp 48.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 144,
+        "url": "sharp - sharp 49.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 145,
+        "url": "sharp - sharp 50.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 146,
+        "url": "sharp - sharp 6.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 147,
+        "url": "sharp - sharp 9.png",
+        "classification": "sharp"
+    },
+    {
+        "id": 148,
+        "url": "sharp - sharp Metal.png",
+        "classification": "sharp"
+    },
+];
+
+export default DEBRIS_CLASSIFICATION_DATA;
